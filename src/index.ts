8fb640c1a5f400b6972138a261d696c67052e636
hedi72 <heditaieb72@gmail.com>
2024-03-14 15:24:50 +0100

    task2

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,10 @@ let arrayThree: (string | number)[] = ["a", 1, "b", 2];
 
 let arrayFour: (string | number | (string | number | boolean)[] )[] = ["a", 1, "b", 2, [1,2], ["a", "b"], ["c", 3], [true, false]];
 
+let myImg = document.getElementById("my-img") as HTMLImageElement;
+console.log(myImg.src);
+
+let data:any = "1000";
+console.log(data);
+
 
